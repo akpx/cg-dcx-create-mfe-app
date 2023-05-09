dea783f71d8a9ea3a666721dac00ea90fbba88e5
Abhishek Pathak <akpathak.us@gmail.com>
2023-05-08 23:22:00 -0400

    add command

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,29 @@
 #!/usr/bin/env node
 
+const { execSync } = require('child-process');
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, {stdio: 'inherit'})
+    } catch (e) {
+        console.error(`Failed to execute command ${command}`, e)
+        return false
+    }
+
+    return true
+}
+
 const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 git@github.com:akpx/cg-dcx-create-mfe-app.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log('Cloning repository');
+const checkedOut = runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit(-1)
+
+console.log('Installing dependencies');
+const installedDeps = runCommand(installDepsCommand);
+if(!installedDeps) process.exit(-1)
+
+console.log(`Congratulations! You are ready. Follow the following commands to start`)
+console.log(`cd ${repoName} && npm start`)
